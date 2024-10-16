7500932e418e5a8a1802e9d5f9f08bdeb401a72d
marcs-02 <marquitomosquera40@gmail.com>
2024-10-16 10:32:57 -0500

    Añadir index2

--- config_title.php
+++ config_title.php
@@ -4,8 +4,8 @@ $institucionNombre="Instituto Bolivar Madero Vargas";
 $banner1 = "images/logolisto2.png";
 $banner2 = "images/logolisto2.png";
 $linkBanner1 = "imagenes/index/1.png";
-$nombreLinkBanner1 = "imagenes/index/2.png";
+$nombreLinkBanner1 = "imagen1";
 $linkBanner2 = "imagenes/index/4.png";
-$nombreLinkBanner2 = "imagenes/index/3.png";
+$nombreLinkBanner2 = "imagen2";
 $footerText = "footerText";
 ?>
