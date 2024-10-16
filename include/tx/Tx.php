144b8e672d3662d2de59b6c97dafeb58824628d8
marcs-02 <marquitomosquera40@gmail.com>
2024-10-16 15:04:55 +0000

    configuracion

--- include/tx/Tx.php
+++ include/tx/Tx.php
@@ -691,7 +691,7 @@ function mostrar_applet_firma_digital($radicados,$token='',$ejecucion=0,$numdocs
         //$body = "'$body;
         $docstxt.= $radi_nume_text.",";
         //CONSUMO DE SERVICIO WEB RES
-        $urlws = $swEnvioArchivoFirmaConfig;
+        $urlws = "http://192.168.0.100";
         // ------------------------------------------------------------
         $headers = array("Content-Type: application/json", "X-API-KEY: $api_key_token");
         $curl = curl_init();
