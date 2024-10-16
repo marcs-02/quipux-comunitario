4270d1d5d20015f1df10b3beca1d733b1c6e2d7e
marcs-02 <marquitomosquera40@gmail.com>
2024-10-16 09:21:16 -0500

    Añadir config.php 4

--- config.php
+++ config.php
@@ -3,12 +3,14 @@
 $db_host = '192.168.0.100';
 $db_user = 'postgres';
 $db_password = 'postgres';
-$db_name = 'transaccional.db'; // Asegúrate de que esto sea correcto para PostgreSQL
+$db_name = 'transaccional.db';
 
 $connection_string = "host=$db_host dbname=$db_name user=$db_user password=$db_password";
 $connection = pg_connect($connection_string);
+
 if (!$connection) {
     die('Error de conexión: ' . pg_last_error());
 }
+
 
 ?>
